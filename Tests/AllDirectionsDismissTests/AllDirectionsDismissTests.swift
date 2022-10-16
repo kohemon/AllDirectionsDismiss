a13f200a898a5701121a0f2df0726f2c139e6411
kohemon <yandapanda1218@gmail.com>
2022-10-16 18:23:29 +0900

    comment out test

--- Tests/AllDirectionsDismissTests/AllDirectionsDismissTests.swift
+++ Tests/AllDirectionsDismissTests/AllDirectionsDismissTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import AllDirectionsDismiss
 
 final class AllDirectionsDismissTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AllDirectionsDismiss().text, "Hello, World!")
-    }
 }
